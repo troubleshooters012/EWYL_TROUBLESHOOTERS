d5a8a491386c5adb6908890a97662b3bb0fb40e6
Roshan22EEE <roshan.b2020eee@sece.ac.in>
2022-05-26 21:16:15 +0530

    task by roshan

--- 26-05-2022/roshan/index.js
+++ 26-05-2022/roshan/index.js
@@ -1,8 +1,11 @@
 function getValue() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+  /*array*/
   console.log([username, password]);
+  /*object*/
   console.log({'name':username, "password":password});
+  /*To show password*/
   var mydetails={"name":username,"password":password};
   console.log(mydetails.password);
   document.getElementById("js").innerHTML=username;
