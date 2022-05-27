f81de8648751d1b19b7d2340a5956b66381321bb
Roshan22EEE <roshan.b2020eee@sece.ac.in>
2022-05-27 11:25:12 +0530

    task by  roshan

--- 26-05-2022/roshan/index.js
+++ 26-05-2022/roshan/index.js
@@ -1,14 +1,15 @@
-function getValue() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  /*array*/
-  console.log([username, password]);
-  /*object*/
-  console.log({'name':username, "password":password});
-  /*To show password*/
-  var mydetails={"name":username,"password":password};
-  console.log(mydetails.password);
-  document.getElementById("js").innerHTML=username;
+
+function getValue(){
+  var username=document.getElementById("username").value;
+  var password=document.getElementById("password").value;
+  var email=document.getElementById("email").value;
+  var mobilenumber=document.getElementById("mobilenumber").value;
+  console.log({username,password,email,mobilenumber});
+
+  document.getElementById("form").innerHTML=username;
+  document.getElementById("go").innerHTML=password;
+  document.getElementById("row").innerHTML=email;
+  document.getElementById("column").innerHTML=mobilenumber;
+  
   
 }
-
